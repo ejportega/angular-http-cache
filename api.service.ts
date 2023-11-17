41f0d99e57e6d7dc05cd576f640afec14706b1d6
Enrique Jose Ortega <40316087+ejportega@users.noreply.github.com>
2023-11-17 09:42:38 +0000

    Update api.service.ts

--- api.service.ts
+++ api.service.ts
@@ -2,7 +2,7 @@ getData(http: HttpClient) {
  return this.http.get('/api/data', {
    context: new HttpContext().set(CACHE_REQUEST , {
      cached: true,
-     id: 1000000 // 10 minutes
+     ttl: 1000000 // 10 minutes
    })
  })
 }
